2a6eb23470fd0b4a3d5a6378e4ce3ba42406bec9
Clarenceeey <clarencey100@gmail.com>
2025-01-01 13:41:15 +0800

    Supply dataset to ai chatbot

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -3,20 +3,20 @@ import getResponses from "@/app/actions/getResponses";
 import { google } from "@ai-sdk/google";
 import { streamText, tool } from "ai";
 import { z } from "zod";
+import { responses } from "@/app/data/responses";
 
 export async function POST(req: Request) {
   const { messages } = await req.json();
+  const data = JSON.stringify(responses);
 
   const result = streamText({
     model: google("gemini-1.5-flash"),
-    system: "You are a helpful assistant.",
+    system: `You are a helpful assistant. Use this dataset to answer queries from users. ${data}`,
     messages,
     tools: {
       count: tool({
         description: "Get the total count of responses",
-        parameters: z.object({
-          count: z.number().describe("The total count of responses"),
-        }),
+        parameters: z.object({}),
         execute: countResponses,
       }),
       getResponses: tool({
